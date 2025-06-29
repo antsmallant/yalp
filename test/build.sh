32d962b25d38019457d043fdc830242620370b17
antsmallant <412288482@qq.com>
2025-06-29 22:47:59 +0800

    sync

--- test/build.sh
+++ test/build.sh
@@ -5,11 +5,12 @@ curdir=$(pwd)
 cd $curdir
 cd ../3rd/lua-5.4.3
 make
+make local
 chmod +x lua
 chmod +x luac
 
 cd $curdir
-../src/luaprofile/
+cd ../src/luaprofile/
 make linux
 cp profile.so $curdir
 
--- test/run_test_profile.sh
+++ test/run_test_profile.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-../3rd/lua-5.4.3/lua test_profile.lua 
+chmod +x ../3rd/lua-5.4.3/install/bin/lua
+../3rd/lua-5.4.3/install/bin/lua test_profile.lua 
